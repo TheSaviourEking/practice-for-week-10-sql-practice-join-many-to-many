f8c956b1468cd0c8f4d6bac4880d889b61726fde
TheSaviourEking <saviour.eking@outlook.com>
2024-02-28 03:18:08 +0100

    chore: implement many-to-many relationship step 2

--- sql-practice/step-2.sql
+++ sql-practice/step-2.sql
@@ -2,3 +2,14 @@
 -- Just like with one-to-many relationships, you can filter one table by any
 -- associated data on a conected table.
 -- Your code here
+SELECT
+  musicians.first_name,
+  musicians.last_name
+FROM
+  musician_instruments
+  JOIN musicians ON (musician_instruments.musician_id = musicians.id)
+  JOIN instruments ON (
+    instruments.id = musician_instruments.instrument_id
+  )
+WHERE
+  instruments.type = 'piano';
